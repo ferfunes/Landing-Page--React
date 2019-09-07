0df0239eda4008d303d8a7b53865bd90cb84a94d
Fernando Funez <ferfunes@gmail.com>
2019-09-07 17:59:34 +0000

    first commit

---
+++ src/js/component/Jumbo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Jumbo = () => {
+	return (
+		<header className="jumbotron my-4">
+			<h1 className="display-3">A Warm Welcome!</h1>
+			<p className="lead" />
+			<a href="#" className="btn btn-primary btn-lg">
+				Call to action!
+			</a>
+		</header>
+	);
+};
